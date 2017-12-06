dc3e2216b41d7f31f012bb23967ab0a516b1f5bc
Mark Youngman <mmyoungman@gmail.com>
2017-12-06 18:50:15 +0000

    Adding solutions for adventofcode 2017 day 6

---
+++ python/adventofcode/2017/06a.py
@@ -0,0 +1,24 @@
+input = [4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3]
+prevConfig = []
+cycles = 0
+
+while input not in prevConfig:
+   prevConfig.append(list(input))
+
+   m = max(input)
+   mpos = 0
+
+   for i, value in enumerate(input):
+      if m == value:
+         mpos = i
+         break
+   input[mpos] = 0
+   
+   while m > 0:
+      mpos += 1
+      input[mpos % len(input)] += 1
+      m -= 1
+
+   cycles += 1
+
+print(cycles)
