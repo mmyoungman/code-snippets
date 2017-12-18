832bb5aab0f57f5680a594dede815587ca2df183
Mark Youngman <mmyoungman@gmail.com>
2017-12-18 23:25:26 +0000

    Solved AoC 2017 Day 18b

--- python/adventofcode/2017/18b.py
+++ python/adventofcode/2017/18b.py
@@ -1,5 +1,3 @@
-# NOTE: Doesn't work!
-
 import sys
 file = open("18input.txt", 'r')
 
@@ -24,71 +22,31 @@ msgs1 = []
 
 sound = 0
 
+def getvalue(arg, values):
+   try:
+      value = int(arg)
+   except:
+      if arg not in values.keys():
+         values[arg] = 0
+      value = values[arg] 
+   return value
+
 def tick(pos, values, sndmsgs, rcvmsgs):
    global sound
    if cmds[pos][0] == "set":
-      if cmds[pos][1] not in values.keys():
-         values[cmds[pos][1]] = 0
-      if cmds[pos][2].lstrip('-').isdigit():
-         values[cmds[pos][1]] = int(cmds[pos][2])
-      else:
-         if cmds[pos][2] not in values.keys():
-            values[cmds[pos][2]] = 0
-         values[cmds[pos][1]] = values[cmds[pos][2]]
-   
-   if cmds[pos][0] == "add":
-      if cmds[pos][1] not in values.keys():
-         values[cmds[pos]     if len(rcvmsgs) < 1:   
-[1]] = 0
-      if cmds[pos][2].lstrip('-').isdigit():
-         values[cmds[pos][1]] += int(cmds[pos][2])
-      else:
-         if cmds[pos][2] not in values.keys():
-            values[cmds[pos][2]] = 0
-         values[cmds[pos][1]] += values[cmds[pos][2]]
-
-   if cmds[pos][0] == "mul":
-      if cmds[pos][1] not in values.keys():
-         values[cmds[pos][1]] = 0
-      if cmds[pos][2].lstrip('-').isdigit():
-         values[cmds[pos][1]] *= int(cmds[pos][2])
-      else:
-         if cmds[pos][2] not in values.keys():
-            values[cmds[pos][2]] = 0
-         values[cmds[pos][1]] *= values[cmds[pos][2]]
-
-   if cmds[pos][0] == "mod":
-      if cmds[pos][1] not in values.keys():
-         values[cmds[pos][1]] = 0
-      if cmds[pos][2].lstrip('-').isdigit():
-         values[cmds[pos][1]] %= int(cmds[pos][2])
-      else:
-         if cmds[pos][2] not in values.keys():
-            values[cmds[pos][2]] = 0
-         values[cmds[pos][1]] %= values[cmds[pos][2]]
-   
-   if cmds[pos][0] == "jgz":
-      jmp = False
-      if cmds[pos][1].lstrip('-').isdigit():
-         if int(cmds[pos][1]) > 0:
-            jmp = True
-      elif values[cmds[pos][1]] > 0:
-         jmp = True
-      
-      if jmp:
-         if cmds[pos][2].lstrip('-').isdigit():
-            pos += int(cmds[pos][2]) - 1
-         else:
-            if cmds[pos][2] not in values.keys():
-               values[cmds[pos][2]] = 0
-            pos += values[cmds[pos][2]] - 1
-
-   if cmds[pos][0] == "snd":
-      if cmds[pos][1] not in values.keys():
-         values[cmds[pos][1]] = 0
-      sndmsgs.append(values[cmds[pos][1]])
-
-   if cmds[pos][0] == "rcv":
+      values[cmds[pos][1]] = getvalue(cmds[pos][2], values)
+   elif cmds[pos][0] == "add":
+      values[cmds[pos][1]] += getvalue(cmds[pos][2], values)
+   elif cmds[pos][0] == "mul":
+      values[cmds[pos][1]] *= getvalue(cmds[pos][2], values)
+   elif cmds[pos][0] == "mod":
+      values[cmds[pos][1]] %= getvalue(cmds[pos][2], values)
+   elif cmds[pos][0] == "jgz":
+      if getvalue(cmds[pos][1], values) > 0:
+         pos += getvalue(cmds[pos][2], values) - 1
+   elif cmds[pos][0] == "snd":
+      sndmsgs.append(getvalue(cmds[pos][1], values))
+   elif cmds[pos][0] == "rcv":
       if len(rcvmsgs) < 1:   
          return pos, values, sndmsgs, rcvmsgs, False
       else:
@@ -107,7 +65,5 @@ while True:
    if len(msgs0) > old:
       sentMsgs0 += 1 
    if (not changed0) and (not changed1):
-      print("DEADLOCK!")
-      print(pos0, pos1)
       print(sentMsgs0)
       sys.exit(0)
