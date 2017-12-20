c189c023928bce0794db12938551bb8eb7f98ea5
Mark Youngman <mmyoungman@gmail.com>
2017-12-20 09:25:17 +0000

    AoC 2017 Day 19 solution

---
+++ python/adventofcode/2017/19a.py
@@ -0,0 +1,52 @@
+import sys
+
+file = open("19input.txt", 'r')
+
+list = []
+
+while True:
+  line = file.readline()
+  if line == '':
+    break
+  line = line.rstrip('\n')
+  list.append(line)
+
+file.close()
+
+posx = 0
+posy = 0
+
+for x in range(len(list[0])):
+   if list[0][x] == "|":
+      posx= x
+      print(x)
+
+dir = "S"
+result = []
+
+while True:
+   if dir == 'N':
+      posy -= 1
+   elif dir == 'S':
+      posy += 1
+   elif dir == 'E':
+      posx += 1
+   else:
+      posx -= 1
+
+   if list[posy][posx] == ' ':
+      print(''.join(result))
+      sys.exit(0)
+   elif list[posy][posx].isalpha():
+      result.append(list[posy][posx])
+   elif list[posy][posx] == '+':
+      if dir == 'N' or dir == 'S':
+         if list[posy][posx+1] != ' ':
+            dir = 'E'
+         else:
+            dir = 'W'
+      elif dir == 'E' or dir == 'W':
+         if list[posy+1][posx] != ' ':
+            dir = 'S'
+         else:
+            dir = 'N'
