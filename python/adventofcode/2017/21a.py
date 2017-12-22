8c48a5830657cd01133e06a54d45d20f54a50395
Mark Youngman <mark@hiveit.co.uk>
2017-12-22 12:57:44 +0000

    AoC 2017 Day 21a Solution

--- python/adventofcode/2017/21a.py
+++ python/adventofcode/2017/21a.py
@@ -18,55 +18,75 @@ for i in range(len(rules)):
    del rules[i][1]
    rules[i][1] = rules[i][1].split('/')
 
-print(rules)
-
 grid = ['.#.', '..#', '###']
 
 def flip(pattern):
-   newpat = pattern
+   newpat = pattern[:]
    for i in range(len(pattern)):
+      row = ''
       for j in range(len(pattern[i])):
-         newpat[i][j] = pattern[i][len(pattern[i]) - j]
+         row += pattern[i][(len(pattern[i])-1) - j]
+      newpat[i] = row
+   # print("flip", pattern, newpat)
+   return newpat
 
 def rotate(pattern):
-   newpat = pattern
-   for i in range(len(pattern)):
-      for j in range(len(pattern)):
-         newpat[j][i] = pattern[i][(len(pattern)-1) - j]
+   newpat = pattern[:]
+   for j in range(len(pattern)):
+      row = ''
+      for i in range(len(pattern)):
+         row += pattern[i][(len(pattern)-1) - j]
+      newpat[j] = row
+   # print("rotate", pattern, newpat)
+   return newpat
 
+def equal(a, b):
+   for i in range(len(a)):
+      if a[i] != b[i]:
+         return False
+   return True
 
 def match(pattern, square):
    if len(pattern) != len(square):
       return False
-   if pattern == square:
+   # print(pattern)
+   if equal(pattern, square):
       return True
    pattern = rotate(pattern)
-   if pattern == square:
+   # print(pattern)
+   if equal(pattern, square):
       return True
    pattern = rotate(pattern)
-   if pattern == square:
+   # print(pattern)
+   if equal(pattern, square):
       return True
    pattern = rotate(pattern)
-   if pattern == square:
+   # print(pattern)
+   if equal(pattern, square):
       return True
    pattern = rotate(pattern)
    pattern = flip(pattern)
-   if pattern == square:
+   # print(pattern)
+   if equal(pattern, square):
       return True
    pattern = rotate(pattern)
-   if pattern == square:
+   # print(pattern)
+   if equal(pattern, square):
       return True
    pattern = rotate(pattern)
-   if pattern == square:
+   # print(pattern)
+   if equal(pattern, square):
       return True
    pattern = rotate(pattern)
-   if pattern == square:
+   # print(pattern)
+   if equal(pattern, square):
       return True
-
    return False
 
 iterations = 5
+pix = 0
 for _ in range(iterations):
+   print(_, grid)
    if len(grid) % 2 == 0:
       pix = 2
    else:
@@ -74,18 +94,25 @@ for _ in range(iterations):
    newgrid = []
    for y in range(len(grid)//pix):
       for x in range(len(grid)//pix):
-         # matchFound = False
+         print(y, x)
+         subgrid = grid[int(y*pix):int((y*pix)+pix)]
+         for i in range(pix):
+            subgrid[i] = subgrid[i][int(x*pix):int((x*pix)+pix)]
+         print("subgrid", subgrid)
+         matchFound = False
          for rule in rules:
-            if match(rule[0], grid[int(y*pix):int((y*pix)+pix)][int(x*pix):int((x*pix)+pix)]):
-               # matchFound = True
+            # if match(rule[0], grid[int(y*pix):int((y*pix)+pix)][int(x*pix):int((x*pix)+pix)]):
+            if match(rule[0], subgrid):
+               print("Match found!")
+               matchFound = True
                if x == 0:
-                  newgrid.append(rule[1]) 
+                  newgrid += rule[1]
                else:
                   for i in range(pix+1):
                      newgrid[(y*(pix+1)) + i] += rule[1][i]
                break
-         # if not matchFound:
-         #    print("Didn't find a match!")
+         if not matchFound:
+            print("Didn't find a match!")
 
    grid = newgrid
 
