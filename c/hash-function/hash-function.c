ed8a98baefad5f657c3fce914b243a27dc927057
Mark Youngman <mmyoungman@gmail.com>
2018-02-17 18:14:48 +0000

    Updated hash-function.c again

--- c/hash-function/hash-function.c
+++ c/hash-function/hash-function.c
@@ -1,7 +1,8 @@
 #include "lib-mmy.h"
 
+int num_buckets = 8;
+
 u64 hash_chars(char* msg) {
-   int num_buckets = 16;
    u64 value = 0;
    u64 expo = 1;
    for(int i = 0; i < str_len(msg); i++) {
@@ -13,7 +14,6 @@ u64 hash_chars(char* msg) {
 }
 
 u64 hash_multrandfloat(char* msg) {
-   int num_buckets = 16;
    u64 value = 0;
    u64 expo = 1;
    for(int i = 0; i < str_len(msg); i++) {
@@ -28,7 +28,6 @@ u64 hash_multrandfloat(char* msg) {
 }
 
 u64 hash_multrand(char* msg) {
-   int num_buckets = 16;
    u64 value = 0;
    u64 expo = 1;
    for(int i = 0; i < str_len(msg); i++) {
@@ -43,7 +42,6 @@ u64 hash_multrand(char* msg) {
 }
 
 u64 hash_crcvariant(char* msg) {
-   int num_buckets = 16;
    u64 value = 0;
    u64 expo = 1;
    for(int i = 0; i < str_len(msg); i++) {
@@ -59,58 +57,84 @@ u64 hash_crcvariant(char* msg) {
    return ((h ^ value) % 2147483647) % num_buckets;
 }
 
+//http://www.burtleburtle.net/bob/hash/hashfaq.html
+u64 hash_bjenkins(char* msg) {
+   u32 h = 0;
+   for(int i = 0; i < str_len(msg); i++) {
+      h += msg[i];
+      h += (h << 10);
+      h ^= (h >> 6);
+   }
+   h += (h << 3);
+   h ^= (h >> 11);
+   h += (h << 15);
+
+   return h % num_buckets;
+}
+
 int main(int argc, char** argv) {
    char* msg = str_copy("aaaaaaaa");
-   int sum_chars[16];
-   for(int i = 0; i < 16; i++) {
-      sum_chars[i] = 0;
-   }
-   for(int i = 0; i < 8; i++) {
-      while(msg[i] < 'z') {
-         sum_chars[hash_chars(msg)] += 1;
-         msg[i] += 1; 
-      }
-   }
-   for(int i = 0; i < 16; i++) {
-      dbg("hash_chars -- bucket: %d, count: %d", i, sum_chars[i]);
-   }
+   int total = 0;
+   //int sum_chars[num_buckets];
+   //for(int i = 0; i < num_buckets; i++) {
+   //   sum_chars[i] = 0;
+   //}
+   //for(int i = 0; i < 8; i++) {
+   //   while(msg[i] < 'z') {
+   //      sum_chars[hash_chars(msg)] += 1;
+   //      msg[i] += 1; 
+   //   }
+   //}
+   //for(int i = 0; i < num_buckets; i++) {
+   //   dbg("hash_chars -- bucket: %d, count: %d", i, sum_chars[i]);
+   //}
+
+   //free(msg);
+   //msg = str_copy("aaaaaaaa");
+   //int sum_multrandfloat[num_buckets];
+   //for(int i = 0; i < num_buckets; i++) {
+   //   sum_multrandfloat[i] = 0;
+   //}
+   //for(int i = 0; i < 8; i++) {
+   //   while(msg[i] < 'z') {
+   //      sum_multrandfloat[hash_multrandfloat(msg)] += 1;
+   //      msg[i] += 1; 
+   //      total += 1;
+   //   }
+   //}
+   //for(int i = 0; i < num_buckets; i++) {
+   //   dbg("hash_multrandfloat -- bucket: %d, count: %d", i, sum_multrandfloat[i]);
+   //}
+   //dbg("hash_multrandfloat total: %d", total);
 
    free(msg);
    msg = str_copy("aaaaaaaa");
-   int sum_multrandfloat[16];
-   for(int i = 0; i < 16; i++) {
-      sum_multrandfloat[i] = 0;
-   }
-   for(int i = 0; i < 8; i++) {
-      while(msg[i] < 'z') {
-         sum_multrandfloat[hash_multrandfloat(msg)] += 1;
-         msg[i] += 1; 
-      }
-   }
-   for(int i = 0; i < 16; i++) {
-      dbg("hash_multrandfloat -- bucket: %d, count: %d", i, sum_multrandfloat[i]);
-   }
-
-   free(msg);
-   msg = str_copy("aaaaaaaa");
-   int sum_multrand[16];
-   for(int i = 0; i < 16; i++) {
+   total = 0;
+   int sum_multrand[num_buckets];
+   for(int i = 0; i < num_buckets; i++) {
       sum_multrand[i] = 0;
    }
    for(int i = 0; i < 8; i++) {
       while(msg[i] < 'z') {
          sum_multrand[hash_multrand(msg)] += 1;
          msg[i] += 1; 
+         total += 1;
       }
    }
-   for(int i = 0; i < 16; i++) {
+   for(int i = 0; i < num_buckets; i++) {
       dbg("hash_multrand-- bucket: %d, count: %d", i, sum_multrand[i]);
    }
+   dbg("hash_multrand total: %d", total);
+   total = 0;
+   for(int i = 0; i < num_buckets; i++) {
+      total += sum_multrand[i];
+   }
+   dbg("hash_multrand calculated total: %d", total);
 
    //free(msg);
    //msg = str_copy("aaaaaaaa");
-   //int sum_crcvariant[16];
-   //for(int i = 0; i < 16; i++) {
+   //int sum_crcvariant[num_buckets];
+   //for(int i = 0; i < num_buckets; i++) {
    //   sum_crcvariant[i] = 0;
    //}
    //for(int i = 0; i < 8; i++) {
@@ -119,7 +143,31 @@ int main(int argc, char** argv) {
    //      msg[i] += 1; 
    //   }
    //}
-   //for(int i = 0; i < 16; i++) {
+   //for(int i = 0; i < num_buckets; i++) {
    //   dbg("hash_crcvariant -- bucket: %d, count: %d", i, sum_crcvariant[i]);
    //}
+
+   free(msg);
+   msg = str_copy("aaaaaaaa");
+   total = 0;
+   int sum_bjenkins[num_buckets];
+   for(int i = 0; i < num_buckets; i++) {
+      sum_bjenkins[i] = 0;
+   }
+   for(int i = 0; i < 8; i++) {
+      while(msg[i] < 'z') {
+         sum_bjenkins[hash_bjenkins(msg)] += 1;
+         msg[i] += 1; 
+         total += 1;
+      }
+   }
+   for(int i = 0; i < num_buckets; i++) {
+      dbg("hash_bjenkins -- bucket: %d, count: %d", i, sum_bjenkins[i]);
+   }
+   dbg("hash_bjenkins total: %d", total);
+   total = 0;
+   for(int i = 0; i < num_buckets; i++) {
+      total += sum_bjenkins[i];
+   }
+   dbg("hash_bjenkins calculated total: %d", total);
 }
