90f4f0eab8670546050c615fde2441c6e9b29c54
Mark Youngman <mark@youngman.info>
2023-10-11 15:05:08 +0100

    go/nostr_backup: refactor event.ToJson to single Sprintf call

--- go/nostr_backup/event.go
+++ go/nostr_backup/event.go
@@ -4,7 +4,6 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
-	"strings"
 )
 
 type Event struct {
@@ -18,19 +17,19 @@ type Event struct {
 }
 
 func (event Event) ToJson() string {
-	var result strings.Builder
-	result.WriteString(fmt.Sprintf("{\"id\":\"%s\",", event.Id))
-	result.WriteString(fmt.Sprintf("\"pubkey\":\"%s\",", event.PubKey))
-	result.WriteString(fmt.Sprintf("\"created_at\":%d,", event.CreatedAt))
-	result.WriteString(fmt.Sprintf("\"kind\":%d,", event.Kind))
-	result.WriteString(fmt.Sprintf("\"tags\":%s,", event.Tags.ToJson()))
-	result.WriteString(
-		fmt.Sprintf("\"content\":%s,", DecorateJsonStr(event.Content)))
-	result.WriteString(fmt.Sprintf("\"sig\":\"%s\"}", event.Sig))
+	result := fmt.Sprintf(
+		"{\"id\":\"%s\",\"pubkey\":\"%s\",\"created_at\":%d,\"kind\":%d,\"tags\":%s,\"content\":%s,\"sig\":\"%s\"}",
+		event.Id,
+		event.PubKey,
+		event.CreatedAt,
+		event.Kind,
+		event.Tags.ToJson(),
+		DecorateJsonStr(event.Content),
+		event.Sig)
 
-	DevBuildValidJson(result.String())
+	DevBuildValidJson(result)
 
-	return result.String()
+	return result
 }
 
 func (event Event) GenerateEventId() string {
