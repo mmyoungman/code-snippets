3cc6ffbc656698c352e54bee7085a01cefc200a2
Mark Youngman <mark@youngman.info>
2024-07-25 20:07:13 +0100

    go/htmx/templ Implement inlineStyleNonce badly

--- go/htmx/templ/middleware/content_security_policy.go
+++ go/htmx/templ/middleware/content_security_policy.go
@@ -1,12 +1,17 @@
 package middleware
 
 import (
+	"mmyoungman/templ/utils"
 	"net/http"
 )
 
 func ContentSecurityPolicy(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Security-Policy", "default-src 'self'")
+
+		// @MarkFix Set random nonce in middleware in r.Context()
+		w.Header().Set("Content-Security-Policy", "default-src 'self'; style-src 'self' 'nonce-" + 
+			utils.Getenv("HTMX_INLINESTYLENONCE") +
+			"'")
 
 		// @MarkFix Add reporting endpoint?
 		//w.Header().Set("Content-Security-Policy", "default-src 'self'; report-to csp-endpoint")
